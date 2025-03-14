1b9a63965e2ffdc8f03c64033ca5ac6720db8f83
Cajetan Dsouza <72744663+Cajetan02@users.noreply.github.com>
2025-03-14 18:44:33 +0000

    Another update :]

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 let index = 0;
 const totalSlides = document.querySelectorAll("#carousel img").length;
 
+
+const myButton = document.getElementById('main');
+if (window.matchMedia("(max-width: 732px)").matches) {
+  
+  myButton.disabled = false;
+} else {
+  myButton.disabled = true;
+}
 // Carousel Functions
 function showSlide(n) {
     index = n;
@@ -11,6 +19,17 @@ function showSlide(n) {
     updateIndicators();
 }
 
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
 function nextSlide() {
     showSlide(index + 1);
 }
